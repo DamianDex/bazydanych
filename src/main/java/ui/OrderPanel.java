3bc561412a27e68fb6b9fbfc3346a9001ed40088
farad <damian.fara@pega.com>
2017-12-16 18:32:31 +0100

    Fix in UI designer

--- src/main/java/ui/OrderPanel.java
+++ src/main/java/ui/OrderPanel.java
@@ -67,7 +67,6 @@ public class OrderPanel extends JPanel {
     private OrdersAbstractTableModel ordersAbstractTableModel;
 
     public OrderPanel() {
-        $$$setupUI$$$();
         initUI();
         addActionListeners();
     }
